ad28675eb46861d6f0794cec6905c61f6027dc75
Talon <1604690196@qq.com>
2022-05-25 10:11:57 +0800

    5 25 更新

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Hello extends Applet {
+    public void init(){
+
+    }
+    public void paint(Graphics g){
+        g.drawString("Hello World!",25,25);
+    }
+}
